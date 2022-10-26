c32502e17159c9ddbf7db53e4596d402b67584f2
henrique <henrique00henk@gmail.com>
2022-10-26 19:51:11 -0400

    feat(att.classes): Implementado classes

---
+++ Att006/Console.py
@@ -0,0 +1,7 @@
+from model import Produto, Compra, Pedido
+
+produtos = []
+
+produtos.append(Produto('Uva', 3.60, 10))
+produtos.append(Produto('Pera', 3.60, 10))
+produtos.append(Produto('Uva', 3.60, 10))
